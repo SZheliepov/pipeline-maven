059184ee0f3ccdf0f0eddc261671ca6846eda769
adv-sergey <44552133+adv-sergey@users.noreply.github.com>
2020-05-02 12:02:28 +0300

    Update deploy.sh

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -5,5 +5,5 @@ echo $BUILD_TAG >> /tmp/.auth
 echo $PASS >> /tmp/.auth
 
 scp -i /opt/prod /tmp/.auth  admin@3.88.18.16:/tmp/.auth
-scp -i /opt/prod ./jenkins/delpoy/publish  admin@3.88.18.16:/tmp/publish
+scp -i /opt/prod ./jenkins/deploy/publish  admin@3.88.18.16:/tmp/publish
 ssh -i /opt/prod admin@3.88.18.16 "/tmp/publish"
