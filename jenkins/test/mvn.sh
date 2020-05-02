06eb823ad15465751a76969afcfa8e08e2551c45
Cloud User <centos@ip-172-31-1-98.ec2.internal>
2020-05-02 08:23:34 +0000

    Fixes workspace path for job

--- jenkins/test/mvn.sh
+++ jenkins/test/mvn.sh
@@ -6,5 +6,6 @@ echo "*************************"
 echo "** Testing the code ********"
 echo "*************************"
 
+WORKSPACE=/home/centos/jenkins/jenkins-data/jenkins_home/workspace/pipeline-maven-docker
 
-docker run   --rm  -v $PWD/java-app:/app -v /root/.m2/:/root/.m2  -w /app maven:3-alpine "$@"
+docker run   --rm  -v $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2  -w /app maven:3-alpine "$@"
